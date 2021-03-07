90a1062e4573a590cf3319933a5fc8ef5a019519
Jong-Pang <53761899+Jong-Pang@users.noreply.github.com>
2021-03-07 18:08:47 +0900

    1 ~ 10번 문제 풀이

---
+++ CodeFestival/01~10/chap10.js
@@ -0,0 +1,17 @@
+function star(n) {
+    let tree = '';
+
+    for (let i = 1; i <= n; i++) {
+        let star = '';
+        for (let j = 1; j <= n - i; j++) {
+            star += ' ';
+        }
+        for (let k = 1; k <= 2 * i - 1; k++) {
+            star += '*';
+        }
+        tree += star + '\n';
+    }
+    return tree;
+}
+
+console.log(star(5));
