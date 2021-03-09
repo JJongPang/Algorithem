50d94ac3124ee69c296f50e56c41c5493c73e226
Jong-Pang <jjongpang92@gmail.com>
2021-03-10 00:51:51 +0900

    chap26 check

--- CodeFestival/21~30/chap26.js
+++ CodeFestival/21~30/chap26.js
@@ -1,42 +1,42 @@
-function solution(str) {
-    switch (str) {
-        case '수성':
-            console.log('Mercury');
-            break;
-        case '금성':
-            console.log('Venus');
-            break;
-        case '지구':
-            console.log('Earth');
-            break;
-        case '화성':
-            console.log('Mars');
-            break;
-        case '목성':
-            console.log('Jupiter');
-            break;
-        case '토성':
-            console.log('Saturn');
-            break;
-        case '천왕성': {
-            console.log('Uranus');
-        }
-        case '해왕성':
-            console.log('Neptune');
-            break;
-        default:
-            console.log(new Error('error'));
-    }
-}
+// function solution(str) {
+//     switch (str) {
+//         case '수성':
+//             console.log('Mercury');
+//             break;
+//         case '금성':
+//             console.log('Venus');
+//             break;
+//         case '지구':
+//             console.log('Earth');
+//             break;
+//         case '화성':
+//             console.log('Mars');
+//             break;
+//         case '목성':
+//             console.log('Jupiter');
+//             break;
+//         case '토성':
+//             console.log('Saturn');
+//             break;
+//         case '천왕성': {
+//             console.log('Uranus');
+//         }
+//         case '해왕성':
+//             console.log('Neptune');
+//             break;
+//         default:
+//             console.log(new Error('error'));
+//     }
+// }
 
-solution('수성');
-solution('금성');
-solution('지구');
-solution('화성');
-solution('목성');
-solution('토성');
-solution('천왕성');
-solution('해왕성');
+// solution('수성');
+// solution('금성');
+// solution('지구');
+// solution('화성');
+// solution('목성');
+// solution('토성');
+// solution('천왕성');
+// solution('해왕성');
 
 const planets = {
     수성: 'Mercury',
@@ -51,8 +51,9 @@ const planets = {
 
 // 해설답안
 
-// function test(str) {
-//     console.log(planets[str]);
-// }
+function test(str) {
+    console.log(planets[str]);
+}
 
-// test('수성');
+test('수성');
+test('지구');
